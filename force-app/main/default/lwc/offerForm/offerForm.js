b301535267dbf884ce67d06e7cd3aafd8edb3318
AnseGit <84069263+AnseGit@users.noreply.github.com>
2024-09-16 13:03:44 +0200

    LightningOutCleanUp

--- force-app/main/default/lwc/offerForm/offerForm.js
+++ force-app/main/default/lwc/offerForm/offerForm.js
@@ -1,57 +1,7 @@
-import { LightningElement, api, wire } from 'lwc';
-//import preview from './preview.html';
-import flow from './Flow.html';
-import { CurrentPageReference } from 'lightning/navigation';
+import { LightningElement} from 'lwc';
 
-export default class /*UserPreferenceWrapper */ OfferForm extends LightningElement {
-  //  _individualId;
-  //  _hashId;
-    showFlow = true;
-    _isInEditorMode = false;
+export default class OfferForm extends LightningElement {
     _inputVariables = [];
-
-    render() {
-        return this._isInEditorMode ? preview : flow; 
-    }
-    @wire(CurrentPageReference)
-    getStateParameters(currentPageReference) {
-       if (currentPageReference) {
-           this._isInEditorMode = currentPageReference.state?.view === 'editor';
-       }
-    }
-
-/*
-    @api
-    get individualId() {
-        return this._individualId;
-    }
-    set individualId(value) {
-        this._individualId = value;
-        if (!!this._individualId) {
-            this._inputVariables.push({
-                name: 'var_T_IndividualId',
-                type: 'String',
-                value: this._individualId
-            });
-            this.showFlow = true;
-        }
-    }
-
-    @api
-    get hashId() {
-        return this._hashId;
-    }
-    set hashId(value) {
-        this._hashId = value;
-        if (!!this._hashId) {
-            this._inputVariables.push({
-                name: 'var_T_HashId',
-                type: 'String',
-                value: this._hashId
-            });
-            this.showFlow = true;
-        }
-    } */
 
     /**
      * Get flow input variables
